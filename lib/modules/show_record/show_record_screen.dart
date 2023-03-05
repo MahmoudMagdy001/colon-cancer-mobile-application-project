3069ff31d701b78354beaddde22c6814144e837f
Mahmoud <urfavdodi@gmail.com>
2023-03-05 22:02:15 +0200

    last

--- lib/modules/show_record/show_record_screen.dart
+++ lib/modules/show_record/show_record_screen.dart
@@ -18,34 +18,40 @@ class showRecordScreen extends StatelessWidget {
 
     return Scaffold(
       appBar: AppBar(
-        title: IconButton(
-            onPressed: () {
-              navigatePushReplacementTo(context, homeLayout());
-            },
-            icon: Icon(Icons.arrow_back_ios_new)),
-        elevation: 5.0,
-      ),
-      body: Padding(
-        padding: EdgeInsets.all(25.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+        title: Row(
           children: [
+            IconButton(
+              onPressed: () {
+                navigatePushReplacementTo(context, homeLayout());
+              },
+              icon: Icon(Icons.arrow_back_ios_new),
+            ),
             Text(
               'Patient #$id',
               style: TextStyle(
-                fontSize: 20.0,
-                fontWeight: FontWeight.bold,
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black87),
+            ),
+          ],
+        ),
+        elevation: 5.0,
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Center(
+              child: Text(
+                'Name: $name',
+                style: TextStyle(
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
             ),
-            SizedBox(height: 10.0),
-            Text(
-              'Name: $name',
-              style: TextStyle(
-                fontSize: 16.0,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            SizedBox(height: 5.0),
+            SizedBox(height: 10),
             Text(
               'Date: $date',
               style: TextStyle(
